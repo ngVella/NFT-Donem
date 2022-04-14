7ada135adcc8c0b9905522b6fbe700520a8aacb8
Bedirhan Şimşek <notemp.bedo@gmail.com>
2022-04-13 19:30:31 -0700

    Transfer

--- src/contracts/ERC721.sol
+++ src/contracts/ERC721.sol
@@ -30,12 +30,15 @@ contract ERC721 {
     //mapping from owner to nomber of owned tokens
     mapping(address => uint256) private _OwnedTokensCount;
 
+    //MApping form tokenId to approved addresses
+    mapping(uint256 => address) private _tokenApprovals;
+
     function balanceOf(address _owner) public view returns (uint256) {
         require(_owner != address(0), "ERC721:Owner query fot non-existens token");
         return _OwnedTokensCount[_owner];
     }
 
-    function ownerOf(uint256 _tokenID) external view returns(address){
+    function ownerOf(uint256 _tokenID) public view returns(address){
         address owner = _tokenOwner[_tokenID];
         require(owner != address(0), "ERC721:Owner query fot non-existens token");
         return owner;
@@ -62,5 +65,32 @@ contract ERC721 {
 
         //for logs
         emit Transfer(address(0), to, tokenID);
+    }
+
+    /// @notice Transfer ownership of an NFT -- THE CALLER IS RESPONSIBLE
+    ///  TO CONFIRM THAT `_to` IS CAPABLE OF RECEIVING NFTS OR ELSE
+    ///  THEY MAY BE PERMANENTLY LOST
+    /// @dev Throws unless `msg.sender` is the current owner, an authorized
+    ///  operator, or the approved address for this NFT. Throws if `_from` is
+    ///  not the current owner. Throws if `_to` is the zero address. Throws if
+    ///  `_tokenId` is not a valid NFT.
+    /// @param _from The current owner of the NFT
+    /// @param _to The new owner
+    /// @param _tokenID The NFT to transfer
+    function _transferFrom(address _from, address _to, uint256 _tokenID) internal{
+        require(_to != address(0), 'ERC721: Transfer to the zero address');
+        require(ownerOf(_tokenID) == _from, 'TERC721: Trying to trasnfer a token address does not own');
+
+        //add the token id to the addres receiving the token
+        _tokenOwner[_tokenID] = _to;
+        
+        //update the balance oh the addtess _from token and _to
+        _OwnedTokensCount[_from] -= 1;
+        _OwnedTokensCount[_to] += 1;
+
+        emit Transfer(_from, _to, _tokenID);
+    }
+    function transferFrom(address _from, address _to, uint256 _tokenID) public{
+        _transferFrom(_from, _to, _tokenID);
     }
 }
